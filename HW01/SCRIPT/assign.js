3f0cc25c9919e44d71e485f2ffd081624a546cce
clearlyhunch <edgecoding@naver.com>
2019-05-03 12:05:53 +0900

    assign.js refactored init

--- HW01/SCRIPT/assign.js
+++ HW01/SCRIPT/assign.js
@@ -7,19 +7,16 @@ function initSetting() {
   let date = dateObj.getDate();
   let day =  new Date(year+'-'+month+'-'+1).getDay();
   let dCount = daysCount(year,month);
-
-  var calBody = document.getElementById("calBody").getElementsByTagName("tr");
-
   document.getElementById("calendarDate").innerHTML = year + '年 ' +month + '月';
 
-  numberInit(calBody,dCount,date, day);
+  numberInit(dCount,date, day);
 }
 
-
-function numberInit(calBody, dCount, date, day) {
-  for (var i=0, count = 1, firstLine; i<6 && count <= dCount ; i++){
+function numberInit(dCount, date, day) {
+  var calBody = document.getElementById("calBody").getElementsByTagName("tr");
+  for (var i=0, count = 1, firstLine; i < 6 && count <= dCount ; i++){
     firstLine = calBody.item(i).getElementsByTagName("td");
-    for(var j=0, tdObj; j<7 && count <= dCount; j++) {
+    for(var j=0, tdObj; j < 7 && count <= dCount; j++) {
       if(day > 0) day -= 1;
       else {
         tdObj = firstLine.item(j);
@@ -31,8 +28,6 @@ function numberInit(calBody, dCount, date, day) {
   }
 }
 
-
-
 function setBackgroundColor(date, tdObj) {
   if(date == tdObj.childNodes[0].nodeValue) {
     tdObj.style.backgroundColor = "#96e3ff";
@@ -41,17 +36,14 @@ function setBackgroundColor(date, tdObj) {
   }else {
     tdObj.style.backgroundColor = "#d9e8ce";
   }
-
 }
 
-function daysCount(year, month){ //월의 일수를 구함
+function daysCount(year, month){
   switch(month){
     case 1: case 3: case 5: case 7: case 8: case 10: case 12:
       return 31;
-
     case 4: case 6: case 9: case 11:
       return 30;
-
     case 2:
       if(((year%400)==0||(year%4)==0&&(year%100)!=0)){
         return 29;
@@ -60,25 +52,4 @@ function daysCount(year, month){ //월의 일수를 구함
         return 28;
       }
   }
-
 }
-
-
-
-// document.getElementById('addList').addEventListener("click", function() {
-//   var itemName = prompt("Enter item name", "w3schools");
-//   if (itemName === null || itemName ==="") {
-//     alert("Are you leaving?");
-//     return;
-//   }
-//   var urlName = prompt("Enter url", "https://www.w3schools.com");
-//   if (urlName === null || urlName ==="") {
-//     alert("Are you really going to leave?");
-//     return;
-//   }
-//   var elem = document.createElement('li');
-//   elem.innerHTML = '<a href="' + urlName + '"target="_blank">' + itemName + "</a>";
-//   document.getElementById('itemList').append(elem);
-//
-//
-// });
